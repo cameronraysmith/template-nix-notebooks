75caa4d00f78c040bbe7a1e520a986cddd524a0b
Cameron Smith <cameron.ray.smith@gmail.com>
2022-05-14 10:37:08 -0400

    update flake
    * reformat
    * add cachix
    * add defaultPackage

--- flake.nix
+++ flake.nix
@@ -1,5 +1,8 @@
 {
-  description = "This is a nix flake for a development environment running jupyter with multiple kernels.";
+  description = "This nix flake produces a development environment running jupyter with multiple kernels.";
+
+  nixConfig.extra-substituters = "https://jupyterwith.cachix.org";
+  nixConfig.extra-trusted-public-keys = "jupyterwith.cachix.org-1:/kDy2B6YEhXGJuNguG1qyqIodMyO4w8KwWH4/vAc7CI=";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -8,77 +11,95 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, stable, jupyterWith, flake-utils }: flake-utils.lib.eachDefaultSystem
-    (system:
+  outputs =
+    inputs@{ self
+    , nixpkgs
+    , stable
+    , jupyterWith
+    , flake-utils }:
+    (flake-utils.lib.eachDefaultSystem
+      (system:
 
-      let
+        let
 
-        pkgs = import nixpkgs {
+          pkgs = import nixpkgs {
 
-          system = system;
-          overlays = nixpkgs.lib.attrValues jupyterWith.overlays;
+            system = system;
+            overlays = nixpkgs.lib.attrValues jupyterWith.overlays ++ [ self.overlay ];
+            config = {
+              allowBroken = true;
+              allowUnfree = true;
+              allowUnsupportedSystem = true;
+            };
 
-        };
+          };
 
-        iPython = pkgs.kernels.iPythonWith {
+          iPython = pkgs.kernels.iPythonWith {
 
-          name = "Python-env";
-          packages = p: with p; [ markdown sympy numpy ];
-          ignoreCollisions = true;
+            name = "Python-env";
+            packages = p: with p; [ markdown sympy numpy ];
+            ignoreCollisions = true;
 
-        };
+          };
 
-        jupyterEnvironment = pkgs.jupyterlabWith {
+          jupyterEnvironment = pkgs.jupyterlabWith {
 
-          kernels = [ iPython ];
+            kernels = [ iPython ];
 
-        };
+          };
 
-        buildImage = pkgs.mkDockerImage {
+          buildImage = pkgs.mkDockerImage {
 
-          name = "template-nix-notebooks";
-          jupyterlab = jupyterEnvironment;
+            name = "template-nix-notebooks";
+            jupyterlab = jupyterEnvironment;
 
-        };
+          };
 
-      in rec {
+        in rec {
 
-        apps.jupyterlab = {
+          apps.jupyterlab = {
 
-          type = "app";
-          program = "${jupyterEnvironment}/bin/jupyter-lab";
+            type = "app";
+            program = "${jupyterEnvironment}/bin/jupyter-lab";
 
-        };
+          };
 
-        defaultApp = apps.jupyterlab;
+          defaultApp = apps.jupyterlab;
 
-        devShell = jupyterEnvironment.env;
+          devShell = jupyterEnvironment.env;
 
-        packages = {
+          packages = {
 
-          ociImage = buildImage;
+            jupyterEnvironment = jupyterEnvironment;
+            ociImage = buildImage;
 
-        };
+          };
 
-        checks = {
+          defaultPackage = self.packages.${system}.jupyterEnvironment;
 
-          markdownlint = pkgs.runCommand "mdl"
-            {
-              buildInputs = with pkgs; [ mdl ];
-            }
-            ''
-              mkdir $out
-              mdl ${./README.md}
-            '';
+          checks = {
 
-          yamllint = pkgs.runCommand "yamllint"
-            {
-              buildInputs = with pkgs; [ yamllint ];
-            }
-            ''
-              mkdir $out
-              yamllint --strict ${./.github/workflows}
-            '';
-        };
-      });
+            markdownlint = pkgs.runCommand "mdl"
+              {
+                buildInputs = with pkgs; [ mdl ];
+              }
+              ''
+                mkdir $out
+                mdl ${./README.md}
+              '';
+
+            yamllint = pkgs.runCommand "yamllint"
+              {
+                buildInputs = with pkgs; [ yamllint ];
+              }
+              ''
+                mkdir $out
+                yamllint --strict ${./.github/workflows}
+              '';
+          };
+         }
+         )
+       ) // {
+         overlay = final: prev: { };
+       };
 }
